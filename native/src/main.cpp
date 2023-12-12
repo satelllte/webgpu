b54bd0498df8dd9a9a56d6aac4517c22b02c6ec9
Yehor <lunaerxs@gmail.com>
2023-12-12 23:22:54 +0200

    First window via GLFW (#1)
    * git submodule add https://github.com/glfw/glfw.git ./native/third-party/glfw
    
    * gitignore clang cache
    
    * consume glfw
    
    * ci: checkout submodules step
    
    * install linux dependencies
    
    * window name: WebGPU
    
    * CI: upload artifact instead of run
    
    * CI: use runner.os in artifact names
    
    * CI: fix upload artifact working directory

--- native/src/main.cpp
+++ native/src/main.cpp
@@ -1,6 +1,24 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main (int, char**) {
-	std::cout << "Hello, world!" << std::endl;
+	if (!glfwInit()) {
+		std::cerr << "Could not initialize GLFW!" << std::endl;
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "WebGPU", NULL, NULL);
+	if (!window) {
+		std::cerr << "Could not open window!" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
